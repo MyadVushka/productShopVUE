1b4f3cec38b63cae2c9b2b80eca7ea8fcf8ed90b
aberfoyle <paolini.by@gmail.com>
2024-03-31 19:03:59 +0400

    add: add catalogue page

---
+++ src/components/CatalogueCard.vue
@@ -0,0 +1,67 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  imgUrl: String,
+  id: Number
+})
+</script>
+
+<template>
+  <div
+    class="catalogue-card"
+    :class="{ 'general-wrapper': props.id === 0 || props.id === 9 || props.id === 11 }"
+  >
+    <div class="img-background">
+      <img :src="props.imgUrl" alt="" />
+      <div class="catalogue-card__background"></div>
+    </div>
+    <div class=" "></div>
+    <p>{{ props.title }}</p>
+  </div>
+</template>
+
+<style scoped>
+.general-wrapper {
+  grid-column: span 2;
+}
+
+.img-background {
+  position: relative;
+  width: 100%;
+}
+
+.img-background img {
+  width: 100%;
+}
+
+.catalogue-card__background {
+  width: 100%;
+  height: 80%;
+  position: absolute;
+  z-index: 100;
+  bottom: 0;
+  background: linear-gradient(0deg, rgba(112, 192, 91, 1) 0%, rgba(112, 192, 91, 0) 100%);
+}
+
+.catalogue-card__background:hover {
+  background: linear-gradient(0deg, rgba(255, 102, 51, 1) 0%, rgba(255, 102, 51, 0) 100%);
+}
+
+.catalogue-card {
+  display: flex;
+  width: 100%;
+
+  position: relative;
+  cursor: pointer;
+  transition: all, 0.5s;
+}
+
+.catalogue-card p {
+  color: white;
+  font-weight: bold;
+  position: absolute;
+  bottom: 5%;
+  left: 5%;
+  z-index: 200;
+}
+</style>
--- src/pages/CataloguePage.vue
+++ src/pages/CataloguePage.vue
@@ -1,9 +1,100 @@
-<script setup></script>
+<script setup>
+import CatalogueCard from '../components/CatalogueCard.vue'
+
+const imgURLS = [
+  {
+    title: 'Молоко, сыр, яйцо',
+    imgUrl: '/public/catalogueIMG/milk.png',
+    id: 0
+  },
+  {
+    title: 'Хлеб',
+    imgUrl: '/public/catalogueIMG/bread.png',
+    id: 1
+  },
+  {
+    title: 'Фрукты и овощи',
+    imgUrl: '/public/catalogueIMG/fruits.png',
+    id: 2
+  },
+  {
+    title: 'Замороженные продукты',
+    imgUrl: '/public/catalogueIMG/frozen-fruits.png',
+    id: 3
+  },
+  {
+    title: 'Напитки',
+    imgUrl: '/public/catalogueIMG/drinks.png',
+    id: 4
+  },
+  {
+    title: 'Кондитерские изделия',
+    imgUrl: '/public/catalogueIMG/bakery.png',
+    id: 5
+  },
+  {
+    title: 'Чай, кофе',
+    imgUrl: '/public/catalogueIMG/coffee&tea.png',
+    id: 6
+  },
+  {
+    title: 'Бакалея',
+    imgUrl: '/public/catalogueIMG/balackley.png',
+    id: 7
+  },
+  {
+    title: 'Здорове питание',
+    imgUrl: '/public/catalogueIMG/sportpit.png',
+    id: 8
+  },
+  {
+    title: 'Зоотовары',
+    imgUrl: '/public/catalogueIMG/zoogoods.png',
+    id: 9
+  },
+  {
+    title: 'Детское питание',
+    imgUrl: '/public/catalogueIMG/childgoods.png',
+    id: 10
+  },
+  {
+    title: 'Мясо, птица, колбаса',
+    imgUrl: '/public/catalogueIMG/meat.png',
+    id: 11
+  },
+  {
+    title: 'Непродовольственные товары',
+    imgUrl: '/public/catalogueIMG/non-food products.png',
+    id: 12
+  }
+]
+</script>
 
 <template>
-    <section></section>
+  <section class="wrapper__info">
+    <div class="innerinfo">
+      <router-link to="/">Главная</router-link>
+      <img src="/public/toPageList.svg" alt="" />
+      <p class="top_paragraph">Каталог</p>
+    </div>
+    <h1 class="main-top-text">Каталог</h1>
+    <div class="catalogue-wrapper">
+      <CatalogueCard
+        v-for="item in imgURLS"
+        :key="item.id"
+        :title="item.title"
+        :imgUrl="item.imgUrl"
+        :id="item.id"
+      />
+    </div>
+  </section>
 </template>
 
 <style scoped>
-
+.catalogue-wrapper {
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 40px;
+}
 </style>
